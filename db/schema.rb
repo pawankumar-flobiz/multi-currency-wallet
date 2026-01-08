0f5ed03c0577e5514e64121130c5afe996517900
pawan kumar <p.pawankumar@flobiz.in>
2026-01-08 15:35:12 +0530

    create table in db or migrate

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2026_01_09_055500) do
 
   # These are extensions that must be enabled in order to support this database
